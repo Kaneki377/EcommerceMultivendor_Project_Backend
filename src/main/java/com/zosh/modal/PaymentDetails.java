bcec183b52c2f94f0980e904183fb5bea007b8ef
Kaneki377 <69791816+Kaneki377@users.noreply.github.com>
2025-07-01 00:44:02 +0700

    First Commit

---
+++ src/main/java/com/zosh/modal/PaymentDetails.java
@@ -0,0 +1,19 @@
+package com.zosh.modal;
+
+import com.zosh.domain.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDetails {
+
+    private String paymentId;
+    private String razorpayPaymentLinkId;
+    private String razorpayPaymentLinkReferenceId;
+    private String razorpayPaymentLinkStatus;
+    private String razorpayPaymentId;
+    private PaymentStatus status;
+}
