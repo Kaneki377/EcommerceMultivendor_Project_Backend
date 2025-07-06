752cedd51e43c359f1d1a2dca54d99f2dd4f1e86
PhatStar <phamtanphat469@gmail.com>
2025-07-06 17:56:33 +0700

    add entity role, repository and test repositoryRole

---
+++ src/test/java/com/zosh/repositoryTest/RoleRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.shopmall.repositoryTest;
+
+import com.shopmall.model.Role;
+import com.shopmall.repository.RoleRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//DataJpaTest Tự động rollback dữ liệu sau mỗi test, nên không ảnh hưởng DB thật
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class RoleRepositoryTest {
+    @Autowired
+    private RoleRepository repo;
+
+    @Test
+    public void testCreateFirstRole() {
+        Role roleAdmin = new Role("Admin"); //Kiểm tra xem việc tạo một Role có lưu thành công vào DB hay không
+        Role saveRole = repo.save(roleAdmin);
+
+        assertThat(saveRole.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateRoles() {
+        Role roleSeller = new Role("Seller");
+
+        Role roleShipper = new Role("Shipper");
+
+        Role roleAssistant = new Role("Manager");
+
+        Role roleKOC = new Role("KOC");
+
+        repo.saveAll(List.of(roleSeller,roleShipper,roleAssistant,roleKOC));
+    }
+}
