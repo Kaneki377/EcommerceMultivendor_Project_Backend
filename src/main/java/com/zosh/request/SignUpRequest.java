846b2dac5266a1bb284c9e9d6e32978301021a44
PhatStar <phamtanphat469@gmail.com>
2025-07-07 20:18:38 +0700

    update AuthController

--- src/main/java/com/zosh/request/SignUpRequest.java
+++ src/main/java/com/zosh/request/SignUpRequest.java
@@ -3,7 +3,5 @@ package com.zosh.response;
 public class SignUpRequest {
     private String email;
     private String fullName;
-    private String password;
-    private String confirmPassword;
-    private String OTP;
+    private String otp;
 }
