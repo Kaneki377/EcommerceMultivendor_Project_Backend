6a4c3395162b4fb194baf6e90c47277a8bf37c06
PhatStar <phamtanphat469@gmail.com>
2025-07-07 18:15:08 +0700

    add AuthController, AuthService,SignupRequest

---
+++ src/main/java/com/zosh/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.zosh.controller;
+
+import com.zosh.model.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    public ResponseEntity<Customer> createCustomerHandler() {
+        return null;
+    }
+}
---
+++ src/main/java/com/zosh/response/SignUpRequest.java
@@ -0,0 +1,9 @@
+package com.zosh.response;
+
+public class SignUpRequest {
+    private String email;
+    private String fullName;
+    private String password;
+    private String confirmPassword;
+    private String OTP;
+}
