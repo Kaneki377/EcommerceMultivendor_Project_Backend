269165a4c18b1172113ea2a483b7f33d6c6924c7
PhatStar <phamtanphat469@gmail.com>
2025-07-08 16:29:46 +0700

    update secret key

--- src/main/java/com/zosh/config/JwtConstant.java
+++ src/main/java/com/zosh/config/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.zosh.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "s";
+    public static final String SECRET_KEY = "wpembytrwcvnryxksdbqwjebruyGHyudqgwveytrtrCSnwifoesarjbwe";
     public static final String JWT_HEADER="Authorization";
 
 }
