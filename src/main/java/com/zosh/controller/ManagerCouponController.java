9fec68c5b0ffc046f0d5a8be8b60fc2770a57cd5
Kaneki377 <69791816+Kaneki377@users.noreply.github.com>
2025-07-24 23:24:54 +0700

    Complete ManagerCouponControllerCouponController

---
+++ src/main/java/com/zosh/controller/ManagerCouponController.java
@@ -0,0 +1,66 @@
+package com.zosh.controller;
+
+import com.zosh.model.Cart;
+import com.zosh.model.Coupon;
+import com.zosh.model.Customer;
+import com.zosh.model.User;
+import com.zosh.service.CartService;
+import com.zosh.service.CouponService;
+import com.zosh.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ManagerCouponController {
+
+    private final CouponService couponService;
+
+    private final CustomerService customerService;
+
+    private final CartService cartService;
+
+    @PostMapping("/apply")
+    public ResponseEntity<Cart> applyCoupon(
+            @RequestParam String apply,
+            @RequestParam String code,
+            @RequestParam double orderValue,
+            @RequestHeader("Authorization"
+            ) String jwt
+    ) throws Exception {
+        Customer customer = customerService.findCustomerByJwtToken(jwt);
+        Cart cart;
+
+        if(apply.equals("true")){
+            cart = couponService.applyCoupon(code, orderValue, customer);
+        }
+        else {
+            cart = couponService.removeCoupon(code, customer);
+        }
+        return ResponseEntity.ok(cart);
+    }
+
+
+    // Admin operations
+    @PostMapping("/admin/create")
+    public ResponseEntity<Coupon> createCoupon(@RequestBody Coupon coupon) {
+        Coupon createdCoupon = couponService.createCoupon(coupon);
+        return ResponseEntity.ok(createdCoupon);
+    }
+
+    @DeleteMapping("/admin/delete/{id}")
+    public ResponseEntity<?> deleteCoupon(@PathVariable Long id) throws Exception {
+        couponService.deleteCoupon(id);
+        return ResponseEntity.ok("Coupon deleted successfully");
+    }
+
+    @GetMapping("/admin/all")
+    public ResponseEntity<List<Coupon>> getAllCoupons() {
+        List<Coupon> coupons = couponService.findAllCoupons();
+        return ResponseEntity.ok(coupons);
+    }
+}
