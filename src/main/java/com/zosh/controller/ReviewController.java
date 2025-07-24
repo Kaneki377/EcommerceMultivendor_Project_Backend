d48c3d4e961a52d7e6ce81c107d30e02ded998c6
Kaneki377 <69791816+Kaneki377@users.noreply.github.com>
2025-07-24 17:26:23 +0700

    Compelete ReviewController

--- src/main/java/com/zosh/controller/ReviewController.java
+++ src/main/java/com/zosh/controller/ReviewController.java
@@ -1,4 +1,87 @@
 package com.zosh.controller;
 
+import com.zosh.model.Customer;
+import com.zosh.model.Product;
+import com.zosh.model.Review;
+import com.zosh.model.User;
+import com.zosh.request.CreateReviewRequest;
+import com.zosh.response.ApiResponse;
+import com.zosh.service.CustomerService;
+import com.zosh.service.ProductService;
+import com.zosh.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.naming.AuthenticationException;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/review")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    private final CustomerService customerService;
+
+    private final ProductService productService;
+
+    @GetMapping("/products/{productId}/reviews")
+    public ResponseEntity<List<Review>> getReviewsByProductId(
+            @PathVariable Long productId) {
+        List<Review> reviews = reviewService.getReviewByProductId(productId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @PostMapping("/products/{productId}/reviews")
+    public ResponseEntity<Review> writeReview(
+            @RequestBody CreateReviewRequest req,
+            @PathVariable Long productId,
+            @RequestHeader("Authorization") String jwt) throws Exception{
+
+        Customer customer = customerService.findCustomerByJwtToken(jwt);
+        Product product = productService.findProductById(productId);
+
+        Review review = reviewService.createReview(
+                req, customer, product
+        );
+        return ResponseEntity.ok(review);
+
+    }
+
+    @PatchMapping("/reviews/{reviewId}")
+    public ResponseEntity<Review> updateReview(
+            @RequestBody CreateReviewRequest req,
+            @PathVariable Long reviewId,
+            @RequestHeader("Authorization") String jwt)
+            throws Exception {
+
+        Customer customer = customerService.findCustomerByJwtToken(jwt);
+
+        Review review = reviewService.updatedReview(
+                reviewId,
+                req.getReviewText(),
+                req.getReviewRating(),
+                customer.getId()
+        );
+        return ResponseEntity.ok(review);
+
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<ApiResponse> deleteReview(
+            @PathVariable Long reviewId,
+            @RequestHeader("Authorization") String jwt) throws
+            Exception {
+
+        Customer customer = customerService.findCustomerByJwtToken(jwt);
+
+        reviewService.deleteReview(reviewId, customer.getId());
+        ApiResponse res = new ApiResponse();
+        res.setMessage("Review deleted successfully");
+
+        return ResponseEntity.ok(res);
+
+    }
 }
