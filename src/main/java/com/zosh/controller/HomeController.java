bf66ef512721fc0be13a5aeaf3d3f07f85a4db02
PhatStar <phamtanphat469@gmail.com>
2025-08-07 19:43:35 +0700

    update home controller

--- src/main/java/com/zosh/controller/HomeController.java
+++ src/main/java/com/zosh/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.zosh.controller;
 
 import com.zosh.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public ApiResponse HomeControllerHandler(){
+    public ResponseEntity<ApiResponse> home(){
         ApiResponse apiResponse = new ApiResponse();
-        apiResponse.setMessage("Weltome to ecommerce multivendor");
-        apiResponse.setMessage("Hello");
-        return apiResponse;
+        apiResponse.setMessage("Ecommerce multi vendor system");
+        return new ResponseEntity<>(apiResponse, HttpStatus.ACCEPTED);
     }
 }
